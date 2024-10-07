f93350aa3de5f62f8f68d2ed79e4c118d764bbca
20083540 <20083540@mail.wit.ie>
2024-10-07 12:15:30 +0100

    Lambda function to get all movies.

---
+++ lambdas/getAllMovies.ts
@@ -0,0 +1,90 @@
+import { Handler } from "aws-lambda";
+
+import { DynamoDBClient } from "@aws-sdk/client-dynamodb";
+//import { DynamoDBDocumentClient, GetCommand } from "@aws-sdk/lib-dynamodb";
+
+
+import { DynamoDBDocumentClient, ScanCommand } from "@aws-sdk/lib-dynamodb";
+
+// Initialization
+const ddbDocClient = createDDbDocClient();
+// Handler
+export const handler: Handler = async (event, context) => {
+  try {
+    console.log("Event: ", JSON.stringify(event));
+    const parameters = event?.queryStringParameters;
+    /*
+    const movieId = parameters ? parseInt(parameters.movieId) : undefined;
+
+    if (!movieId) {
+      return {
+        statusCode: 404,
+        headers: {
+          "content-type": "application/json",
+        },
+        body: JSON.stringify({ Message: "Missing movie Id" }),
+      };
+    }
+*/
+
+    const commandOutput = await ddbDocClient.send(
+
+      /*
+      new GetCommand({
+        TableName: process.env.TABLE_NAME,
+        Key: { id: movieId },
+      })
+      */
+
+      new ScanCommand({
+        TableName: process.env.TABLE_NAME,
+      })
+
+    );
+    console.log('GetCommand response: ', commandOutput)  // NEW
+    if (!commandOutput.Items) {
+      return {
+        statusCode: 404,
+        headers: {
+          "content-type": "application/json",
+        },
+        body: JSON.stringify({ Message: "Invalid movie Id" }),
+      };
+    }
+    const body = {
+      data: commandOutput.Items,
+    };
+
+    // Return Response
+    return {
+      statusCode: 200,
+      headers: {
+        "content-type": "application/json",
+      },
+      body: JSON.stringify(body),
+    };
+  } catch (error: any) {
+    console.log(JSON.stringify(error));
+    return {
+      statusCode: 500,
+      headers: {
+        "content-type": "application/json",
+      },
+      body: JSON.stringify({ error }),
+    };
+  }
+};
+
+function createDDbDocClient() {
+  const ddbClient = new DynamoDBClient({ region: process.env.REGION });
+  const marshallOptions = {
+    convertEmptyValues: true,
+    removeUndefinedValues: true,
+    convertClassInstanceToMap: true,
+  };
+  const unmarshallOptions = {
+    wrapNumbers: false,
+  };
+  const translateConfig = { marshallOptions, unmarshallOptions };
+  return DynamoDBDocumentClient.from(ddbClient, translateConfig);
+}
--- lib/simple-app-stack.ts
+++ lib/simple-app-stack.ts
@@ -13,6 +13,7 @@ export class SimpleAppStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
+    //This is for the simple function. Relates to ./lambdas/simple.ts
     const simpleFn = new lambdanode.NodejsFunction(this, "SimpleFn", {
       architecture: lambda.Architecture.ARM_64,
       runtime: lambda.Runtime.NODEJS_18_X,
@@ -27,7 +28,9 @@ export class SimpleAppStack extends cdk.Stack {
         allowedOrigins: ["*"],
       },
     });
+    //End simple function relating to ./lambds/simple.ts
 
+    //Movie database.
     const moviesTable = new dynamodb.Table(this, "MoviesTable", {
       billingMode: dynamodb.BillingMode.PAY_PER_REQUEST,
       partitionKey: { name: "id", type: dynamodb.AttributeType.NUMBER },
@@ -35,6 +38,7 @@ export class SimpleAppStack extends cdk.Stack {
       tableName: "Movies",
     });
 
+    //This is for the getMovieByID() function, relating to ./lambas/getMovieByID.ts
     const getMovieByIdFn = new lambdanode.NodejsFunction(
       this,
       "GetMovieByIdFn",
@@ -57,10 +61,41 @@ export class SimpleAppStack extends cdk.Stack {
         allowedOrigins: ["*"],
       },
     });
+    //End function declarations for getMovieById() function.
 
+    //This is for the getAllMovies() function for exercise Week 4. Relates to ./lambdas/getAllMovies.ts
+    const getAllMoviesFn = new lambdanode.NodejsFunction(
+      this,
+      "GetAllMoviesFn",
+      {
+        architecture: lambda.Architecture.ARM_64,
+        runtime: lambda.Runtime.NODEJS_18_X,
+        entry: `${__dirname}/../lambdas/getAllMovies.ts`,
+        timeout: cdk.Duration.seconds(10),
+        memorySize: 128,
+        environment: {
+          TABLE_NAME: moviesTable.tableName,
+          REGION: 'eu-west-1',
+        },
+      }
+    );
+
+    const getAllMoviesURL = getAllMoviesFn.addFunctionUrl({
+      authType: lambda.FunctionUrlAuthType.NONE,
+      cors: {
+        allowedOrigins: ["*"],
+      },
+    });
+    //End function declarations for getAllMovies() functions.
+
+
+    //Permissions
     moviesTable.grantReadData(getMovieByIdFn)
+    moviesTable.grantReadData(getAllMoviesFn);
 
 
+
+    //Initializes database.
     new custom.AwsCustomResource(this, "moviesddbInitData", {
       onCreate: {
         service: "DynamoDB",
@@ -77,8 +112,10 @@ export class SimpleAppStack extends cdk.Stack {
       }),
     });
 
+    //output for function to CloudWatch(??)
     new cdk.CfnOutput(this, "Simple Function Url", { value: simpleFnURL.url });
     new cdk.CfnOutput(this, "Get Movie Function Url", { value: getMovieByIdURL.url });
+    new cdk.CfnOutput(this, "Get All Movies Url", { value: getAllMoviesURL.url});
 
   }
 }
